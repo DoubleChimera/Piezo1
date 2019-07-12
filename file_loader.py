ca0b1716d99a4067055d3813a842cf386c37673c
Cake <52736573+DoubleChimera@users.noreply.github.com>
2019-07-11 18:38:45 -0700

    Reorganization before coding loader

---
+++ file_loader.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# todo Step 1:  Load a .json file, a corresponding .tif file, set an output directory,
+# todo          and take in initial inputs for the file
+
