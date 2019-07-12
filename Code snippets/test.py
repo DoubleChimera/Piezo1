2f6d8426342acbff2ce7dc58e5fbed548c4c46db
Cake <52736573+DoubleChimera@users.noreply.github.com>
2019-07-11 18:02:32 -0700

    Updated style to PEP8

--- Code snippets/test.py
+++ Code snippets/test.py
@@ -1,17 +1,18 @@
-import numpy as np
 import math
-
-NaN = np.NaN
-
-x1 = np.array([1, 2, 3, 4, 5])
-x2 = np.array([2, 4, 8, 16, 32])
-lagtime = 1
-moment = 2
+import numpy as np
 
 
-msd = (np.nansum((x1[lagtime:] - x1[:-lagtime])**moment) / len(x1[lagtime:]))
-rmsd = math.sqrt(msd)
+NAN = np.nan
 
-print(msd)
-print(rmsd)
-print(math.sqrt(np.nansum(x1[:])))
+X1 = np.array([1, 2, 3, 4, 5])
+X2 = np.array([2, 4, 8, 16, 32])
+LAGTIME = 1
+MOMENT = 2
+
+
+MSD = (np.nansum((X1[LAGTIME:] - X1[:-LAGTIME])**MOMENT) / len(X1[LAGTIME:]))
+RMSD = math.sqrt(MSD)
+
+print(MSD)
+print(RMSD)
+print(math.sqrt(np.nansum(X1[:])))
