ba3161fc0eaab8f0eb0a723b5c1ea214bc3e7af2
CakeUCI <52736573+DoubleChimera@users.noreply.github.com>
2019-08-08 19:03:49 -0700

    stupid nan

--- stat_Histograms.py
+++ stat_Histograms.py
@@ -45,22 +45,89 @@ if __name__ == '__main__':
     pos_columns = None
     if pos_columns is None:
         pos_columns = ['x', 'y']
-    lagtimes = [1, 2, 3]
-    lag_columns = ['_lag{}'.format(l) for l in lagtimes]
+
 
     def genLagColumns(lag_columns, pos_columns):
         for lag in lag_columns:
             for p in pos_columns:
                 yield ''.join(map(str, (p, lag)))
-    results = list(genLagColumns(lag_columns, pos_columns))
 
-    def genDisplacements(tracks, pixelWidth, pos_columns=None):
+    # Generates all displacement for a single track and returns the resulting DF
+    def indivTrackDispls(track, pixelWidth, frameTime, maxLagTime, pos_columns=None):
         if pos_columns is None:
             pos_columns = ['x', 'y']
-        return None
+        # Make a list of tracks and determine the max track length
+        for lag in range(int(track['frame'].max())):
+            return None
+
+    # Sets the index of tracks DF to 'particle' # ! Unnecessary?
+    # tracks.set_index('particle', inplace=True)
+
+    # Determines max track length and gens maxLagtimes List
+    maxLagTime = list(range(tracks.groupby('particle').count().max()[0]))
+    # Determines total number of particles in DF
+    totalParticleCount = tracks.groupby('frame').count().max()[0]
+
+    # self.pos = self.pos.reindex(np.arange(self.pos.index[0],
+    #                                       1 + self.pos.index[-1]))
+
+    # ! THIS WORKS FOR A SINGLE TRAJECTORY
+    # for particle, track in tracks.reset_index(drop=True).groupby('particle'):
+    #     # Gen lagt column and insert in 2nd position
+    #     track.insert(2, 'lagt', (track['frame'] * frameTime), True)
+    #     # Gen suffix of lag columns
+    #     lag_columns = ['_lag{}'.format(l) for l in maxLagTime]
+    #     # Use pos_columns to gen prefix of lag columns
+    #     lag_results = list(genLagColumns(lag_columns, pos_columns))
+    #     # Add the lag columns to what will be the output results df
+    #     results = track.reindex(columns = track.columns.tolist() + lag_results)
+    #     # run some function that takes in a single track and calcs disps
+    #     trackDisps = indivTrackDispls(track, pixelWidth, frameTime, maxLagTime)
+    #     # results = results.append(trackDisps, sort=False)
+
+    #     for lag in range(int(len(track['frame']))):
+    #         if lag == 0:
+    #             results[[f'x_lag{lag}', f'y_lag{lag}']] = track[pos_columns]
+    #         else:
+    #             indivDisp = track[pos_columns].values[lag:] - track[pos_columns].values[:-lag]
+    #             results[[f'x_lag{lag}']] = pd.Series(indivDisp[:,0])
+    #             results[[f'y_lag{lag}']] = pd.Series(indivDisp[:,1])
+    # print(results)
+    # ! END OF SINGLE TRAJECTORY SUCCESS STORY
+    # ? test
+    # print(tracks.set_index('particle').loc[0])
+    # ? End test
+
+    tracks.insert(2, 'lagt', (tracks['frame'] * frameTime))
+    lag_columns = ['_lag{}'.format(l) for l in maxLagTime]
+    lag_results = list(genLagColumns(lag_columns, pos_columns))
+    results = tracks.reindex(columns = tracks.columns.tolist() + lag_results)
+    results.set_index('particle', inplace=True)
+    # print(results.loc[0])
+    # results.loc[0][['x_lag99', 'y_lag99']] = [[35, 35]]
+
+    for particle, track in tracks.reset_index(drop=True).groupby('particle'):
+        trackLength = int(len(track['frame']))
+        for lag in range(trackLength):
+            if lag == 0:
+                results.loc[particle][[f'x_lag{lag}',f'y_lag{lag}']] = track[pos_columns].values
+            else:
+                indivDisp = pd.DataFrame(track[pos_columns].values[lag:] - track[pos_columns].values[:-lag])
+                # results.loc[particle][[f'x_lag{lag}']] = pd.Series(indivDisp[:,0])
+                # results.loc[particle][[f'y_lag{lag}']] = pd.Series(indivDisp[:,1])
+        print(results['x_lag0'])
+
+
+        # msds.append(stat.msdNan(track, pixelWidth, frameTime, max_lagtime, pos_columns))
+        # ids.append(particle)
+
+
+    # takes a track and changes the pixels to microns # ! Keepable
+    # pos = track.set_index('frame')[pos_columns] * pixelWidth
+
 
     # Take in tracks
-    # Adjust values with pixelWidth
+    # Adjust values with pixelWidth so distances are in microns
     # Determine the longest track length in tracks
     #   Use this to determine max columns of lag
     # Loop over each particles trajectory
