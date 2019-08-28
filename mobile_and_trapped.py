e3c12b8c0a7335fafa6ac214bc63bc91ba7847e1
CakeUCI <52736573+DoubleChimera@users.noreply.github.com>
2019-08-28 12:50:28 -0700

    Setting up new file of methods

---
+++ mobile_and_trapped.py
@@ -0,0 +1,14 @@
+# Load a .json of all tracks
+# Use selection criterion to differentiate mobile and trapped tracks
+# Plot Mobile and Trapped Tracks with output
+# TAMSD of mobile tracks
+# --Power Law fit
+# --Slope Fit
+# EAMSD of mobile tracks
+# --Power Law fit
+# --Slope Fit
+# TAMSD and EAMSD mobile tracks, both on same plot
+# --Linear Fit
+# --Linear Fit on Log Data
+# Plot any individual mobile or trapped track to visualize its path
+# Distribution of alpha values TAMSD for upto 10% trajectory time
