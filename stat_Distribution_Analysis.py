dc0fe08efa44b3e50c429eef0048e6770e4c2c30
CakeUCI <52736573+DoubleChimera@users.noreply.github.com>
2019-09-20 15:04:18 -0700

    cumulative distribution done, need fitting Algo

--- stat_Distribution_Analysis.py
+++ stat_Distribution_Analysis.py
@@ -6,6 +6,7 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import stat_MSD_outputs as statMSDo
 
 
 class json_converter(object):
@@ -132,7 +133,7 @@ if __name__ == "__main__":
     # Load tracks data into a pandas DataFrame
     selectedTracks_DF = jc.json_SelectedTracks_to_DF(jsonSelectedTracksLoadPath)
     TAMSD_DF = pd.read_json(jsonTAMSDLoadPath, orient="split")
-    AllTracksLags_DF = pd.read_json(jsonAllTracksAllLags, orient="split")
+    AllTracksAllLags_DF = pd.read_json(jsonAllTracksAllLags, orient="split")
     MobileTrappedTracks_Dict = jc.load_MobileTrapped_json(jsonMobileTrappedDictPath)
 
     # Histogram the alpha values for the TAMSD of Mobile Tracks up to a cutoff percentage
@@ -141,12 +142,125 @@ if __name__ == "__main__":
     # * -----  END SUBROUTINE  ----- * #
 
     # ! ----- START DEBUGGING  ----- ! #
+    # Cumulative Distributon Function, lagtime_limit is in number of frames and inclusive
+    def cumulDistrib(
+        AllTracksAllLags_DF,
+        MobileTracks_List,
+        frameTime,
+        lagtime_limit=10,
+        outputPlotRange=1,
+    ):
+        # Renames the index of AllTracksAllLags_DF to 'particle'
+        AllTracksAllLags_DF.index.names = ["particle"]
+        # Gen mobileTracksAllLags_DF upto the lagtime_limit var
+        mobileTracksAllLags_DF = AllTracksAllLags_DF.loc[
+            MobileTracks_List, "lagt":f"y_lag{lagtime_limit}"
+        ]
+        # Convert 'lagt' column to frames by multiplying by 10 and casting as int
+        mobileTracksAllLags_DF.loc[:, "lagt"] *= 10
+        mobileTracksAllLags_DF.lagt = mobileTracksAllLags_DF.lagt.astype(int)
+        # Rename 'lagt' column to 'frame'
+        mobileTracksAllLags_DF.rename(columns={"lagt": "frame"}, inplace=True)
+        # Set 'particle' and 'frame' as the indices for mobileTracksAllLags_DF
+        mobileTracksAllLags_DF.reset_index(inplace=True)
+        mobileTracksAllLags_DF.set_index(["particle", "frame"], inplace=True)
+        # make a list of all unique indices corresponding to particle numbers, use that list as the index for this DF.
+        particle_List = mobileTracksAllLags_DF.reset_index().particle.unique()
+        # Gen an empty DF for r^2 and CDF results
+        cumulDistrib_DF = pd.DataFrame(particle_List, columns=["particle"]).set_index(
+            "particle"
+        )
+        # Setup the prefix and suffix for the column names
+        CDF_columns_prefix = ["r2", "CDF"]
+        CDF_columns_suffix = ["_lag{}".format(l) for l in range(lagtime_limit + 1)]
+        # Iterate over the prefixes and suffixes to produce the column names list
+        stat_MSD = statMSDo.stat_MSD()
+        CDF_columns_results = list(
+            stat_MSD.genLagColumns(CDF_columns_suffix, CDF_columns_prefix)
+        )
+        # Delete the useless 'CDF_lag0' column name
+        CDF_columns_results.remove("CDF_lag0")
+        # Append the columns names list to the cumulDistrib_DF
+        cumulDistrib_DF = cumulDistrib_DF.reindex(
+            columns=cumulDistrib_DF.columns.tolist() + CDF_columns_results
+        )
 
+        for particle in particle_List:
+            # Calculate and insert all r^2 values into cumulDistrib_DF
+            for lag in range(lagtime_limit + 1):
+                # for every lag, find the r^2,
+                xy_vals = np.array(
+                    mobileTracksAllLags_DF.loc[
+                        (particle, 0), [f"x_lag{lag}", f"y_lag{lag}"]
+                    ]
+                )
+                r2_temp = (xy_vals[0] ** 2) + (xy_vals[1] ** 2)
+                # Store result in cumulDistrib_DF in r2_lag{lag}
+                cumulDistrib_DF.at[particle, f"r2_lag{lag}"] = r2_temp
+        # At this point all the cumulDistrib lags have been inserted
+        # Now calc all cumulative distribution function values
+        # Loop over particle list
+        for particle in particle_List:
+            # Loop over each lag in particle list
+            for lag in np.arange(lagtime_limit) + 1:
+                # Determine total number of tracks, for later division
+                CDF_total_tracks = cumulDistrib_DF[f"r2_lag{lag}"].count()
+                # Extract value to compare against the rest of the list of values
+                r2_to_compare = float(cumulDistrib_DF.loc[particle, [f"r2_lag{lag}"]])
+                # Determine how many values are less than or equal to it
+                CDF_temp = (cumulDistrib_DF[f"r2_lag{lag}"] <= r2_to_compare).sum()
+                # Divide that total by the number of tracks
+                CDF_temp = CDF_temp / CDF_total_tracks
+                # Insert that value in the appropriate place in the cumulDistrib_DF
+                cumulDistrib_DF.at[particle, f"CDF_lag{lag}"] = CDF_temp
+        # Plot the cumulative distribution function
+        for plotNum in np.arange(outputPlotRange) + 1:
+            # Grab the columns of interest
+            plotCDF_DF = cumulDistrib_DF[
+                [f"r2_lag{plotNum}", f"CDF_lag{plotNum}"]
+            ].copy()
+            # Sort by the r2 column
+            plotCDF_DF = plotCDF_DF.sort_values(
+                [f"r2_lag{plotNum}", f"CDF_lag{plotNum}"], ascending=[True, False]
+            )
+            print(plotCDF_DF)
+            # Plot the two columns with r2 as 'x' and CDF as 'y'
+            plt.step(plotCDF_DF[f"r2_lag{plotNum}"], plotCDF_DF[f"CDF_lag{plotNum}"])
+            # ! Need to pretty up this plot with nice formatting
+            plt.show()
+
+    cumulDistrib(AllTracksAllLags_DF, MobileTrappedTracks_Dict["Mobile"], frameTime)
+    # ! -----  END DEBUGGING   ----- ! #
+
+    # * ----- SCRAP CODE BELOW FOR REFERENCE----- * #
+"""
     # Use the mobileTracks_List to make a DF of mobile TAMSD
     mobileTAMSDTracks_DF = TAMSD_DF.loc[:, MobileTrappedTracks_Dict["Mobile"]]
+
     # Insert the 'lagt' column into the mobileTAMSD DF
     mobileTAMSDTracks_DF.insert(0, "lagt", TAMSD_DF["lagt"], True)
+
     # Print the mobile TAMSD DF
     print(mobileTAMSDTracks_DF)
+"""
+"""
+    # This accesses the 0th particle, with all tracks and all lags
+    # print(AllTracksLags_DF.loc[0])
 
-    # ! -----  END DEBUGGING   ----- ! #
+    # This accesses a specific column, such as x_lag1 or y_lag1 of that data
+    # particularly, this slices all the data in the represented range
+    # print(AllTracksLags_DF.loc[0, 'x_lag0':'y_lag2'])
+
+    # Use the mobileTracks_List to make a DF of mobile TAMSD
+    # mobileTAMSDTracks_DF = TAMSD_DF.loc[:, MobileTrappedTracks_Dict["Mobile"]]
+
+    # Insert the 'lagt' column into the mobileTAMSD DF
+    # mobileTAMSDTracks_DF.insert(0, "lagt", TAMSD_DF["lagt"], True)
+
+    # Print the mobile TAMSD DF with 'lagt' as an index
+    # print(mobileTAMSDTracks_DF.set_index('lagt'))
+
+    # for column in mobileTAMSDTracks_DF.set_index('lagt'):
+    #     print(column)
+
+"""
